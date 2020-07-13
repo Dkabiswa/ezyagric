d9cf08743c58d47416ddd3bd4416dcefb9fd3f02
Kabiswa Davis <kabiswad@gmail.com>
2020-07-13 11:52:42 +0200

    use env variable for mailgun api

--- index.py
+++ index.py
@@ -32,31 +32,28 @@ def purchase():
     itemId = request.get_json().get('itemId')
     for item in store:
         for key, values in item.items():
-            print(key, values)
             if key == 'id':
                 if values == itemId:
                     soldItem = item
                     break
     if not soldItem:
         return jsonify({'message': 'item does not exist'})
-
     emailData = {
         "name": request.get_json().get('name'),
-        "item": soldItem.name,
         "paymentOption": request.get_json().get('paymentOption'),
-        "itemName": soldItem.name,
-        "itemPrice": soldItem.price,
-        "total": request.get_json().get('quantity') * soldItem.price,
+        "itemName": soldItem['item'],
+        "itemPrice": soldItem['price'],
+        "total": request.get_json().get('quantity') * soldItem['price'],
     }
-
+    # SEND AN EMAIL WITH SOLD ITEM DETAILS 
     return requests.post(
 		"https://api.mailgun.net/v3/sandboxc124857d76b943478004fec6bdbdfe76.mailgun.org/messages",
-		auth=("api", "07545992aa71ac27330cc627a5126ee2-9dfbeecd-da67d304"),
+		auth=("api", maigunapi),
 		data={"from": "Mailgun Sandbox <postmaster@sandboxc124857d76b943478004fec6bdbdfe76.mailgun.org>",
 			"to": "Davis Kabiswa <dkabitswa@gmail.com>",
 			"subject": "Ezy Agric item Purchase",
 			"template": "provider",
-			"h:X-Mailgun-Variables": json.dumps(emailData, indent='\t')})
+			"h:X-Mailgun-Variables": jsonify(emailData)})
 
 if __name__ == '__main__':
     app.run()
